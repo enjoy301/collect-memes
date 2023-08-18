7fe4972ac3a50a4228f107d1bfa0aa87b2f329b0
Eunjong Kim <eunjong147@pusan.ac.kr>
2023-08-18 10:33:50 +0000

    fix: header위에 feed보이는 현상 수정

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <div className="w-screen h-16 sticky bg-slate-400 sm:h-[80px] sm:fixed" />
+    <div className="w-screen h-16 sticky bg-slate-400 sm:h-[80px] sm:fixed z-50" />
   );
 };
 
