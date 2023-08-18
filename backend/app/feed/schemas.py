388fc2f74d85f23bd3c0df8334080b99b0c5022b
Eunjong Kim <eunjong147@pusan.ac.kr>
2023-08-18 10:34:38 +0000

    refactor: Feed model에 is_like 추가, FeedLike 모델 추가

--- backend/app/feed/schemas.py
+++ backend/app/feed/schemas.py
@@ -4,8 +4,14 @@ from pydantic import BaseModel
 class Feed(BaseModel):
     provider: str
     link: str
+    is_like: bool | None = False
     video_id: str | None = None
 
 
 class FeedInDB(Feed):
     id: str
+
+
+class FeedLike(BaseModel):
+    id: str
+    is_like: bool
