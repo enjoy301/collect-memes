5475be58314f57e189dd276f397c950d55b6809d
Eunjong Kim <eunjong147@pusan.ac.kr>
2023-08-19 03:54:58 +0000

    chore: add new secret key

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    AWS_ACCESS_KEY_ID: str
+    AWS_SECRET_ACCESS_KEY: str
+    AWS_S3_BUCKET_URL: str
+
     class Config:
         env_file = "../.env"
 
